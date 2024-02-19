8e338e0903daa3dd72a7b9255a61dc62b77705ad
Bekzod <bekzodamirov600@gmail.com>
2024-02-19 21:35:02 +0500

    write view

--- api/bekzod/views.py
+++ api/bekzod/views.py
@@ -1,8 +1,8 @@
 from .serializers import AttendanceSerializers
 from rest_framework import generics
+from apps.structure.models import Attendance
+from apps.base.enum import UserRol
 
 
-class AttendanceUpdateView(generics.RetrieveAPIView):
+class AttendanceView(generics.ListAPIView):
     serializer_class = AttendanceSerializers
-
-    
