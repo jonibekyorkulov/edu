0d7d3b18719ee1048984d04916f0cb2cc50b55b0
Javlonbek Boriyev <153982223+Javlonbek3388@users.noreply.github.com>
2024-02-19 21:08:12 +0500

    Update

--- api/javlonbek/serializers.py
+++ api/javlonbek/serializers.py
@@ -7,11 +7,3 @@ class AttendanceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Attendance
         fields = '__all__'
-
-
-class GradeSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Attendance
-        fields = ['task_id', 'student_id', 'grade']
-
