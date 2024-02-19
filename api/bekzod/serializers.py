1a0a5febcb7a71129e2c85acf1050f163b11976f
Bekzod <bekzodamirov600@gmail.com>
2024-02-19 21:02:05 +0500

    write nb

--- api/bekzod/serializers.py
+++ api/bekzod/serializers.py
@@ -1,9 +1,8 @@
-from ...apps.accounts.models import User
+from apps.structure.models import Attendance
 from rest_framework import serializers
 
 
 class AttendanceSerializers(serializers.ModelSerializer):
-    status = serializers.BooleanField(default=False)
     class Meta:
-        model = User
-        fields = ('id', 'teacher_id', 'lesson_id', 'status')
+        model = Attendance
+        fields = ('id', 'student_id', 'lesson_id', 'status')
