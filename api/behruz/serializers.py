9207964de487167acf6b6b9d4c68dff3eb538cf2
bekiake <behruzatoyev00@gmail.com>
2024-02-20 21:38:35 +0500

    task_submit

--- api/behruz/serializers.py
+++ api/behruz/serializers.py
@@ -5,4 +5,6 @@ from rest_framework.exceptions import ValidationError
 class StudentTaskSubmittionsSerializer(serializers.Serializer):
     
     file = serializers.FileField(required=True)
+    task_id = serializers.CharField(max_length=255, required=True)
+    
     
