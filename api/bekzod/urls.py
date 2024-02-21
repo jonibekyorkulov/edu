965180ccbb211a6c2690cc0e81e565062b80f036
Bekzod <bekzodamirov600@gmail.com>
2024-02-21 17:13:45 +0500

    write views

--- api/bekzod/urls.py
+++ api/bekzod/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from api.bekzod.views import AttendanceUpdateView
+from api.bekzod.views import AttendanceListCreateApiView, AttendanceRetrieveView
 
 urlpatterns = [
+    path('nb/', AttendanceUpdateView.as_view()),
     
 ]
--- api/bekzod/views.py
+++ api/bekzod/views.py
@@ -1,8 +1,8 @@
 from .serializers import AttendanceSerializers
 from rest_framework import generics
-from apps.structure.models import Attendance
-from apps.base.enum import UserRol
 
 
-class AttendanceView(generics.ListAPIView):
+class AttendanceUpdateView(generics.RetrieveAPIView):
     serializer_class = AttendanceSerializers
+
+    
