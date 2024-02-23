7528cdef90fba1d1b27d51ec089b0fc9fcf1b876
Sohib Nutfulloyev <mr.nut.sohib@gmail.com>
2024-02-23 20:34:40 +0500

    postgresga otkazish

--- core/settings.py
+++ core/settings.py
@@ -139,23 +139,23 @@ REST_FRAMEWORK = {
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': env.str('NAME'), 
-#         'USER': env.str('USER'),
-#         'PASSWORD': env.str('PASSWORD'),
-#         'HOST': env.str('HOST'), 
-#         'PORT': '5432',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': env.str('NAME'), 
+        'USER': env.str('USER'),
+        'PASSWORD': env.str('PASSWORD'),
+        'HOST': env.str('HOST'), 
+        'PORT': '5432',
+    }
+}
 
 
 # Password validation
