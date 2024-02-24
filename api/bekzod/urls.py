62bd9bd2961f9e006a3bc1c8a5b476b33be85d3e
bekzodamirovdev <bekzodamirov600@gmail.com>
2024-02-24 11:32:13 +0500

    write views

--- api/bekzod/urls.py
+++ api/bekzod/urls.py
@@ -1,3 +1,7 @@
+from django.urls import path
+from api.bekzod.views import AttendanceRetrieveApiView
+
 urlpatterns = [
+    path('attendance-retrieve', AttendanceRetrieveApiView.as_view(), name='attendance-retrieve'),
     
 ]
--- api/bekzod/views.py
+++ api/bekzod/views.py
@@ -1,8 +1,21 @@
 from .serializers import AttendanceSerializers
-from rest_framework import generics
+from rest_framework import generics, status, permissions
 from apps.structure.models import Attendance
-from apps.base.enum import UserRol
+from rest_framework.response import Response
 
 
-class AttendanceView(generics.ListAPIView):
+class AttendanceRetrieveApiView(generics.RetrieveAPIView):
     serializer_class = AttendanceSerializers
+    permission_classes = [permissions.IsAdminUser, ]
+
+
+    def get_queryset(self):
+        return Attendance.objects.all()
+    
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        instance.status = False
+        instance.save()
+        sz = self.get_serializer(instance)
+        return Response(sz.data, status=status.HTTP_200_OK)
+    
