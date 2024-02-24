f8b2391840b05806a7b9c0e5c5165a4a87fd0266
bekzodamirovdev <bekzodamirov600@gmail.com>
2024-02-24 11:24:51 +0500

    write views

--- api/bekzod/urls.py
+++ api/bekzod/urls.py
@@ -1,3 +1,6 @@
+from django.urls import path
+from api.bekzod.views import AttendanceRetrieveApiView
+
 urlpatterns = [
-    
+    path('attendance-retrieve', AttendanceRetrieveApiView.as_view(), name='attendance-retrieve'),
 ]
