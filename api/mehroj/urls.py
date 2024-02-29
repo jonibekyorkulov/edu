d1e6b3f0ae35c7501dbda7c2f9a983b739c2fe53
s1rojiddinnov1c <s1rojiddinnov1c@gmail.com>
2024-02-29 18:03:42 +0500

    ,.,.

--- api/mehroj/urls.py
+++ api/mehroj/urls.py
@@ -2,13 +2,6 @@ from django.urls import path
 from .views import CreateRoomView, CreateGroupView,ListBaholarView
 
 
-# urlpatterns =[
-#     path('create_group/',CreateGroupView.as_view()),
-#     path('update_group/',UpdateGroupView.as_view()),
-#     path('retrieve_group/',RetrieveGroupView.as_view()),
-#     path('delete_group/', DeleteGroupView.as_view()),
-# ]
-###### XONA YARATISH 
 urlpatterns =[
     path('create_room/', CreateRoomView.as_view()),
     path('create_group/',CreateGroupView.as_view()),
