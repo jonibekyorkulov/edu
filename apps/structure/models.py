ac394e4c64a648d359658f328364a296dee0b7ae
Abdulaziz <abdulazizmusurmonov204@gmail.com>
2024-03-01 13:55:49 +0500

    nothing

--- apps/structure/models.py
+++ apps/structure/models.py
@@ -50,9 +50,9 @@ class Group(BaseModel):
     def __str__(self) -> str:
         return self.name
 
-    def save(self, *args,**kwargs):
-        self.end_time = self.start_time + timedelta(months = self.subject_id.duration)
-        super(Group, self).save(*args,**kwargs)
+    # def save(self, *args,**kwargs):
+    #     self.end_time = self.start_time + timedelta(months = self.subject_id.duration)
+    #     super(Group, self).save(*args,**kwargs)
 
 class Lesson(BaseModel):
 
